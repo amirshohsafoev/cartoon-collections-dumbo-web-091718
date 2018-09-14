4214e057a1e5e0c8d4c0ff9bfe82c1049566ae32
Amir Safoev <amirsafoev0@gmail.com>
2018-09-14 15:49:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) 
+  var string = ""
+  for (var i = dwarves.length/2; i < dwarves.length; i++){
+
+     string += `${i + 1}. ${dwarves[i]} `
+string.each do |x|
+  puts x\n 
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planets) 
+ 
+  planets.collect do |n|
+    n.capitalize + "!"
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planets) 
+  
+  planets.any? do |planet|
+    planet.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
